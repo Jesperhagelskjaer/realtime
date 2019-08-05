4de866319a7f6f287e46bf6d64d84286f5f50db7
Jesperhagelskjaer <jhag@dandrite.au.dk>
2019-08-05 15:49:00 +0200

    05082019

--- effect/plotOneTrace.m
+++ effect/plotOneTrace.m
@@ -3,16 +3,17 @@ function [] = plotOneTrace(data,TTL_number)
 for i = 1:size(data,2)
     h = figure;
     plot(data(:,i,TTL_number))
-    color = get(h,'Color');
-    %set(gca,'XColor',color,'YColor',color,'TickDir','out')
+    %color = get(h,'Color');
+    set(gcf,'color','w');
+    set(gca,'XColor',[1 1 1],'YColor',[1 1 1],'TickDir','out')
     %set(gca,'YColor',color,'TickDir','out')
     
-    set(gca,'Visible','off')
-    xticks([0 6000])
-    xticklabels({'0','200'})
-    xlabel('Time [ms]')
-    box off
-  
+   % set(gca,'Visible','off')
+    %xticks([0 6000])
+    %xticklabels({'0','200'})
+    %xlabel('Time [ms]')
+    %box off
+    
 end
 
 
